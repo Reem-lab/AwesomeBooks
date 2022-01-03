aa28d9fb5ed8f0db5864b1160d8866a040bf23c5
Reem <janinareem@gmail.com>
2022-01-03 22:16:15 +0200

    Add the arry of objects

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('.add');
+
+
+const books = [
+    {
+        title : 'Natural',
+        author : 'Reem',
+    },
+    {
+        title : 'intro to js',
+        author : 'Fernando',
+    },
+]
+
+
+
+
+btn.addEventListener('click', function(e){
+    e.preventDefault();
+     
+});

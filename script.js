5512e231ffb47133fa3e8a55688b42ff7073fcf6
fherrerao <fherrera0206@gmail.com>
2022-01-03 17:51:38 -0500

    Fix eventlistener

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
+
 const btn = document.querySelector('.add');
 const heading = document.querySelector('.heading');
-const remove = document.querySelector('.remove');
-
-
-
-
-
-/*const titleBook = document.createElement('label');
-titleBook.classList.add('titleBook');
-titleBook.appendChild(bookContainer);
-
-const authorBook = document.createElement('label');
-authorBook.classList.add('authorBook');
-authorBook.appendChild(bookContainer);*/
-
 
 const books = [
     {
@@ -34,30 +21,35 @@ const displayInformation = function (){
         <p class="info" id="titleBook">${books[i].title}</p>
         <div class="empty-div"></div>
         <p class="info" id="authorBook">${books[i].author}</p>
-        <button class="remove">Remove</button>
+        <button id="remove" class="remove">Remove</button>
         `;
         heading.append(bookContainer);
     }
 }
 
-
+document.addEventListener('DOMContentLoaded', () => {
+        displayInformation();
+        const remove = document.querySelector('#remove');
+        remove.addEventListener('click', () => {
+        alert("HIIII");
+    });
+});
 
 btn.addEventListener('click', function(e){
     e.preventDefault();
-   console.log(e.target)
-   const title = document.querySelector('.title').value;
-   const author = document.querySelector('.author').value;
+    console.log(e.target)
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
     books.push({title , author})
     displayInformation();
 });
 
-// document.addEventListener('load',function(){
-//     displayInformation();
-// });
+
+/*var remove = function(removeID){
+    var index = array.indexOf(removeID);
+    if (index>-1) {
+        array.splice(index, 1);
+    }
+}*/
 
 
-   remove.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log(e.target());
-    //heading.classList.remove('bookContainer')
-});

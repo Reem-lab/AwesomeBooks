96bc272b5d535d40d86344c0a14165ff122f0bf9
fherrerao <fherrera0206@gmail.com>
2022-01-05 15:05:08 -0500

    Check repetition of book

--- script.js
+++ script.js
@@ -50,14 +50,46 @@ class Book {
       this.showBook(book);
     }
 }
+function addMessage() {
+  const alert = document.createElement('p');  
+  const parent = document.querySelector('.btnAdd');
+  alert.textContent = "Book already added";
+  alert.classList.add('alert');
+  parent.appendChild(alert);
+}
+
+function checkRepetition(book) {
+  for (let i = 0; i < Book.collection.length; i += 1) {
+    const currentBook = Book.collection[i];
+    if (currentBook.title.toLowerCase() === book.title.toLowerCase()
+    && currentBook.author.toLowerCase() === book.author.toLowerCase()) {
+      setTimeout(addMessage(), 3000);     
+      
+      //alert('Book already added');
+      return false;
+    }
+  }
+  return true;
+}
+
+function addBookToLibrary() {
+  const inputTitleValue = inpTitle.value;
+  const inputAuthorValue = inpAutor.value;
+  const book = new Book(inputTitleValue, inputAuthorValue);
+  if (checkRepetition(book)) {
+    book.saveBooks();
+  }
+}
+
 const book = new Book();
 const form = document.querySelector('.form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  book.saveBooks();
+  addBookToLibrary();
   inpTitle.value = '';
   inpAutor.value = '';
 });
+
 document.addEventListener('DOMContentLoaded', () => {
   if (localStorage.getItem('books') !== null) {
     const myBooks = JSON.parse(localStorage.getItem('books'));

a2137d722b65d68737289cee538b4d0def0b6f08
fherrerao <fherrera0206@gmail.com>
2022-01-05 10:25:52 -0500

    Fix linters errors

--- script.js
+++ script.js
@@ -2,74 +2,73 @@ const bookContainer = document.querySelector('.book-container');
 const inpTitle = document.querySelector('#title');
 const inpAutor = document.querySelector('#author');
 
-
 class Book {
     static collection = [];
 
-    constructor(title, author){
-        this.title = title;
-        this.author = author;
+    constructor(title, author) {
+      this.title = title;
+      this.author = author;
     }
 
     static setLocalStorage=() => {
-        const data = JSON.stringify(Book.collection);
-        localStorage.setItem('books', data);
+      const data = JSON.stringify(Book.collection);
+      localStorage.setItem('books', data);
     }
 
     deleteBook = (title, author) => {
-        Book.collection = Book.collection.filter((book) => book.title !== title || book.author !== author);
+      Book.collection = Book.collection.filter((book) => book.title !== title
+      || book.author !== author);
     }
 
     showBook(book) {
-        const bookDiv = document.createElement('DIV');
-        const bookTitle = document.createElement('p');
-        const deleteBtn = document.createElement('BUTTON');
-        deleteBtn.classList.add('btn-remove');
+      const bookDiv = document.createElement('DIV');
+      const bookTitle = document.createElement('p');
+      const deleteBtn = document.createElement('BUTTON');
+      deleteBtn.classList.add('btn-remove');
 
-        let title = '"' + book.title + '" by ' + book.author;
-        bookTitle.textContent = title;
+      const title = `"${book.title}" by ${book.author}`;
+      bookTitle.textContent = title;
 
-        deleteBtn.textContent = 'Remove';
+      deleteBtn.textContent = 'Remove';
 
-        bookDiv.appendChild(bookTitle);   
-        bookDiv.appendChild(deleteBtn);
-        bookContainer.appendChild(bookDiv);
-        
-        deleteBtn.addEventListener('click', () => {
-            this.deleteBook(book.title, book.author);
-            Book.setLocalStorage(Book.collection);
-            bookContainer.removeChild(bookDiv);
-        });
+      bookDiv.appendChild(bookTitle);
+      bookDiv.appendChild(deleteBtn);
+      bookContainer.appendChild(bookDiv);
+
+      deleteBtn.addEventListener('click', () => {
+        this.deleteBook(book.title, book.author);
+        Book.setLocalStorage(Book.collection);
+        bookContainer.removeChild(bookDiv);
+      });
     }
 
     iterateBooks() {
-        Book.collection.forEach((book) => {
-          this.showBook(book);
-        });
+      Book.collection.forEach((book) => {
+        this.showBook(book);
+      });
     }
 
-    saveBooks () {
-        const book = new Book(inpTitle.value, inpAutor.value);
-        Book.collection.push(book);
-        Book.setLocalStorage(Book.collection);
-        this.showBook(book);
+    saveBooks() {
+      const book = new Book(inpTitle.value, inpAutor.value);
+      Book.collection.push(book);
+      Book.setLocalStorage(Book.collection);
+      this.showBook(book);
     }
 }
 
-  const book = new Book();
-  const form = document.querySelector('.form');
-    form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    book.saveBooks();
-    inpTitle.value = '';
-    inpAutor.value = '';
+const book = new Book();
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  book.saveBooks();
+  inpTitle.value = '';
+  inpAutor.value = '';
 });
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('books') !== null) {
-        const myBooks = JSON.parse(localStorage.getItem('books'));
-        Book.collection = myBooks;
-    }
-    book.iterateBooks();
+  if (localStorage.getItem('books') !== null) {
+    const myBooks = JSON.parse(localStorage.getItem('books'));
+    Book.collection = myBooks;
+  }
+  book.iterateBooks();
 });
